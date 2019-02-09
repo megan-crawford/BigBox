4c98bcc0bfdca7bf26eef7bfb003b2d1a1272bf9
kdcrafter <kurtisdoubled@hotmail.com>
2019-02-09 00:54:47 -0500

    Added register view and form validation checking:

--- Main/forms.py
+++ Main/forms.py
@@ -1,8 +1,37 @@
 from django import forms
+from django.core.exceptions import ValidationError 
+from django.core.validators import EmailValidator, RegexValidator
+from . models import Users
+from re import search #regex
 
 class RegisterForm(forms.Form):
-    email = forms.CharField(max_length=254)
-    password = forms.CharField(max_length=128, widget=forms.PasswordInput)
-    first_name = forms.CharField(max_length=35)
-    last_name = forms.CharField(max_length=35)
-    location = forms.CharField(max_length=200) #form type may need to be updated
+    email = forms.EmailField(label='Email Address')
+    password = forms.CharField(label='Password', max_length=128, widget=forms.PasswordInput)
+    password_confirmation = forms.CharField(label='Password Confirmation', max_length=128, widget=forms.PasswordInput)
+    first_name = forms.CharField(label='First Name', max_length=35)
+    last_name = forms.CharField(label='Last Name', max_length=35)
+    location = forms.CharField(label='Location', max_length=200) #form type may need to be updated
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        
+        if not Users.objects.filter(email=email).exists:
+            raise ValidationError(message='Email already exists', code='preexisting_email')
+
+        return email
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data['first_name']
+        
+        if search('^[A-Za-z]+$', first_name) is None:
+            raise ValidationError(message='First name can only contain letters', code='invalid_name')
+
+        return first_name
+
+    def clean_last_name(self):
+        last_name = self.cleaned_data['last_name']
+        
+        if search('^[A-Za-z]+$', last_name) is None:
+            raise ValidationError(message='Last name can only contain letters', code='invalid_name')
+
+        return last_name
--- Main/views.py
+++ Main/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import FormView
 from .forms import RegisterForm
+from django.contrib.auth import login, logout
+from . models import Users
+
+def register(request):
+    if request.method == "POST": #user clicks register button
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            #get form data
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            location = form.cleaned_data['location']
+
+            #create and add user to database
+            user = Users.objects.create(email=email, password=password,first_name=first_name, last_name=last_name, location=location)
+            user.save()
+            login(request, user)
+
+            redirect('home/')
+        else:
+            render(request, 'Main/register.html', {'form':form})
+
+    else: #user is viewing the register page
+        form = RegisterForm()
+        render(request, 'Main/register.html', {'form':form})
