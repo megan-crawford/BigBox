a082e1c913e30caad6397810e1b0ec8f94938cee
Jason <jbagnara@purdue.edu>
2019-02-16 18:36:06 -0600

    fixed syntax errors

--- Main/models.py
+++ Main/models.py
@@ -1,52 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
-    ID = models.BigIntegerField()
-    Email = models.TextField()
-    Password = models.TextField()
-    FirstName = models.TextField()
-    Description = models.TextField()
-    Age = models.SmallIntegerField()
-    #image
-    Contacts = models.ManyToManyField(User)
-    Reports = models.ManyToManyField(Report)
-
-class Report(models.Model):
-    PAYMENT = 'PI'
-    VIOLENCE = 'V'
-    NOSHOW = 'NS'
-    SCAM = 'S'
-    OTHER = 'O'
-
-    REPORT_CHOICES = (    
-        (PAYMENT, 'Payment Issue'),
-        (VIOLENCE, 'Violence'),
-        (NOSHOW, 'No show'),
-        (SCAM, 'Scam'),
-        (OTHER, 'Other'),
-    )
-
-    ID = models.BigIntegerField()
-    Classification = models.CharField(
-        choices=REPORT_CHOICES
-    )
-    Details = models.TextField()
-
-class Seeker(User):     #Job Seeker, subclass to User
-    
-    PrefType = models.ManyToManyField(Post.TYPE_CHOICES)
-    IntJob = models.ManyToManyField(Post)
-    Reviews = models.ManyToManyField(Review)
-    Location = models.TextField()
-
-class Creator(User):    #Job Creator
-    Posts = models.ManyToManyField(Post)
-    Reviews = models.ManyToManyField(Review)
-
-class Review(models.Model):
-    ID = models.BigIntegerField()
-    Rating = models.SmallIntegerField() #Precision undecided
 
 class Post(models.Model):
     LAWNMOWING='LM'
@@ -69,10 +23,68 @@ class Post(models.Model):
             (OTHER, 'Other'),
     )
 
-    ID = models.BigIntegerField()
     Pay = models.FloatField()
     Location = models.TextField()
     DateTime = models.DateTimeField()
-    Interested = models.ManyToManyField(Seeker)
+    #Interested = models.ManyToManyField(Seeker)
     Description = models.TextField()
-    JobType = models.ManyToManyField(TYPE_CHOICES)    
+    JobType = models.CharField(
+            max_length=100,
+            choices=TYPE_CHOICES,
+    )
+
+class Report(models.Model):
+    PAYMENT = 'PI'
+    VIOLENCE = 'V'
+    NOSHOW = 'NS'
+    SCAM = 'S'
+    OTHER = 'O'
+
+    REPORT_CHOICES = (    
+        (PAYMENT, 'Payment Issue'),
+        (VIOLENCE, 'Violence'),
+        (NOSHOW, 'No show'),
+        (SCAM, 'Scam'),
+        (OTHER, 'Other'),
+    )
+
+    Classification = models.CharField(
+        max_length=100,
+        choices=REPORT_CHOICES
+    )
+    Details = models.TextField()
+
+class User(models.Model):
+    Email = models.TextField()
+    Password = models.TextField()
+    FirstName = models.TextField()
+    Description = models.TextField()
+    Age = models.SmallIntegerField()
+    #image
+    Contacts = models.ManyToManyField("self")
+    Reports = models.ForeignKey(Report, on_delete=models.CASCADE)
+
+class JobChoices(models.Model):
+    Types = models.CharField(
+            max_length=100,
+            choices=Post.TYPE_CHOICES,
+    )
+            
+class Review(models.Model):
+    Rating = models.SmallIntegerField() #Precision undecided
+
+class Seeker(User):     #Job Seeker, subclass to User
+    
+    PrefType = models.ForeignKey(
+            JobChoices,
+            on_delete=models.CASCADE,
+    )
+    IntJob = models.ManyToManyField(Post)
+    Reviews = models.ManyToManyField(Review)
+    Location = models.TextField()
+
+class Creator(User):    #Job Creator
+    Posts = models.ManyToManyField(Post)
+    Reviews = models.ManyToManyField(Review)
+
+
