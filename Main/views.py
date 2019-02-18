31596c122b72adbe445f9fce425211335d62af0b
kdcrafter <kurtisdoubled@hotmail.com>
2019-02-18 16:46:36 -0500

    fixed create account view error

--- Main/views.py
+++ Main/views.py
@@ -1,42 +1,31 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import FormView
-from .forms import RegisterForm
+from .forms import CreateAccountForm
 from django.contrib.auth import login, logout
 from django.http import HttpResponse
 from . models import User
 
-def register(request):
+def create_account(request):
     if request.method == "POST": #user clicks register button
-        form = RegisterForm(request.POST)
+        form = CreateAccountForm(request.POST)
 
         if form.is_valid():
-            #get form data
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password']
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            location = form.cleaned_data['location']
-
-            #create and add user to database
-            user = User.objects.create(email=email, password=password,first_name=first_name, last_name=last_name, location=location)
-            user.save()
-            login(request, user)
-
-            redirect('home/')
+            form.save()
+            return redirect('home/')
         else:
-            render(request, 'Main/register.html', {'form':form})
+            return render(request, 'Create Account/createAccount.html', {'form':form})
 
-    else: #user is viewing the register page
-        form = RegisterForm()
-        render(request, 'Main/register.html', {'form':form})
+    else: #user is viewing the create account page
+        form = CreateAccountForm()
+        return render(request, 'Create Account/createAccount.html', {'form':form})
 
 #home page
 def home(request):
     return render(request, 'main/login.html')
     #return HttpResponse("home.")
 
-def create_account(request):
-    return render(request, 'Create Account/createAccount.html')
+# def create_account(request):
+#     return render(request, 'Create Account/createAccount.html')
 
 #redirect to home    
 def logout_request(request):
