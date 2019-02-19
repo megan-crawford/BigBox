62213f3c5d2aa4c3ebeabb6716f693e47bd8b856
kdcrafter <kurtisdoubled@hotmail.com>
2019-02-19 18:25:22 -0500

    merged css for real

--- Main/views.py
+++ Main/views.py
@@ -1,36 +1,24 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import FormView
-from .forms import RegisterForm
+from .forms import CreateAccountForm
 from django.contrib.auth import login, logout
 from django.http import HttpResponse
 #from . models import Users
 
-"""
-def register(request):
+def create_account(request):
     if request.method == "POST": #user clicks register button
-        form = RegisterForm(request.POST)
+        form = CreateAccountForm(request.POST)
 
         if form.is_valid():
-            #get form data
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password']
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            location = form.cleaned_data['location']
-
-            #create and add user to database
-            user = Users.objects.create(email=email, password=password,first_name=first_name, last_name=last_name, location=location)
-            user.save()
-            login(request, user)
-
-            redirect('home/')
+            form.save()
+            return redirect('home/')
         else:
-            render(request, 'Main/register.html', {'form':form})
+            return render(request, 'Create Account/createAccount.html', {'form':form})
 
-    else: #user is viewing the register page
-        form = RegisterForm()
-        render(request, 'Main/register.html', {'form':form})
-"""
+    else: #user is viewing the create account page
+        form = CreateAccountForm()
+        return render(request, 'Create Account/createAccount.html', {'form':form})
+
 #home page
 def home(request):
     return render(request, 'login.html')
@@ -43,5 +31,7 @@ def logout_request(request):
 
 #create_job page
 def create_job(request):
-    return render(request, 'bigBoxJob.html')
-    #return HttpResponse("job.")
+    return render(request, 'Jobs/bigBoxJob.html')
+
+def list_job(request):
+    return render(request, 'Jobs/listJobs.html')
