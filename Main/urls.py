1799485f7f85a1ef9a744a475bf2d40af861e48d
Megan <megseees@gmail.com>
2019-02-19 19:24:05 -0500

    page redirection after submitting create job form

--- Main/urls.py
+++ Main/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
 	path('create_account', views.create_account, name='create_account'),
     path('create_job', views.create_job, name='create_job'),
     path('list_job', views.list_job, name='list_job'),
+    path('add_job', views.new_job, name='add_job'),
 ]
