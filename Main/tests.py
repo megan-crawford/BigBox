cd00727f21c501715954f9dcc129aad9aa970240
Jason <jbagnara@purdue.edu>
2019-03-08 16:42:24 -0600

    omg testing wow

--- Main/tests.py
+++ Main/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from . models import Review
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class UserTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='1234')
+        login = self.client.login(username='testuser', password='1234')
+        Review.objects.create(Rating = 20)
+
+    def test_user(self):
+        self.client.login(email="a", password="1234")
+        self.assertEqual(Review.objects.get(Rating=20).Rating, 19)
+        #self.assertTrue(True)
